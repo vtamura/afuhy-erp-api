903cbd52f1fa31f7fe2379a150b04b13ab706c41
vtamura <vtamura@cervejariacidadeimperial.com.br>
2026-06-15 23:01:19 -0300

    chore: configure Prettier

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+    endOfLine: 'lf',
+    printWidth: 80,
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSameLine: false,
+    bracketSpacing: true,
+}
+
+module.exports = config
